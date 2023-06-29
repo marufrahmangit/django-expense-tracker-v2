b2ee71022cad2f5c526d693b8e9575090bd638ad
Maruf Rahman <marufmunem@gmail.com>
2023-06-29 11:09:40 +0600

    tweaking app for vercel

--- config/settings.py
+++ config/settings.py
@@ -74,10 +74,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'URL': 'postgresql://postgres:Kxd925DkAxbDd8T5ba65@containers-us-west-39.railway.app:8064/railway',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'Kxd925DkAxbDd8T5ba65',
+        'HOST': 'containers-us-west-39.railway.app',
+        'PORT': 'PGPORT',
     }
 }
 
